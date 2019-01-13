cb16a80a34194bc888efa79a8c4817f34a90666c
Francis Karagu <fkaragu@gmail.com>
2019-01-13 17:57:13 +0300

    Added custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { HighlightthroughDirective } from './highlightthrough.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    HighlightthroughDirective
+    HighlightthroughDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
