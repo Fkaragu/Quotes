f5bb55ff61160613272d6f3843ae5f9810307956
Francis Karagu <fkaragu@gmail.com>
2019-01-13 16:22:12 +0300

    Added class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Quotes';
+    quote: string[];
+
+  constructor(){
+
+    this.quote = ["Watch Finding Nemo","Buy Cookies","Get new phone case"];
+  }
 }
