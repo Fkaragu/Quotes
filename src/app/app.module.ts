0694411c9715ec44a55189613edf13a4a232fce7
Francis Karagu <fkaragu@gmail.com>
2019-01-13 17:39:52 +0300

    Added custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { HighlightthroughDirective } from './highlightthrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    HighlightthroughDirective
   ],
   imports: [
     BrowserModule,
