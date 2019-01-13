5976b035a50afc486d3abbce78805c634786d963
Francis Karagu <fkaragu@gmail.com>
2019-01-13 16:48:34 +0300

    Added property binding

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,14 +8,14 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
 
-    quote = [
-        new Quote(1,'Watch Finding Nemo'),
-        new Quote(2,'Buy Cookies'),
-        new Quote(3, 'Get new Phone Case'),
-        new Quote(4, 'Get Dog Food'),
-        new Quote(5, 'Solve math homework'),
-        new Quote(6, 'Plot my world domination plan'),
-    ]
+  quote = [
+      new Quote(1,'Watch Finding Nemo','Find an online version and watch merlin find his son'),
+      new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
+      new Quote(3, 'Get new Phone Case','Diana has her birthday coming up soon'),
+      new Quote(4, 'Get Dog Food','Pupper likes expensive sancks'),
+      new Quote(5, 'Solve math homework','Damn Math'),
+      new Quote(6, 'Plot my world domination plan','Cause I am an evil overlord'),
+  ]
 
   constructor() { }
 
