9e515d9f3b0fb417d343617fb5195e6883f9fc80
Francis Karagu <fkaragu@gmail.com>
2019-01-14 05:19:25 +0300

    Added quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,10 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
 
   quote = [
-        new Quote(1,'Watch Finding Nemo','Find an online version and watch merlin find his son',new Date(2018,3,14) ),
-        new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2018,6,9) ),
-        new Quote(3, 'Get new Phone Case','Diana has her birthday coming up soon',new Date(2018,1,12) ),
-        new Quote(4, 'Get Dog Food','Pupper likes expensive sancks',new Date(2018,0,18) ),
-        new Quote(5, 'Solve math homework','Damn Math',new Date(2018,2,14) ),
-        new Quote(6, 'Plot my world domination plan','Cause I am an evil overlord',new Date(2018,3,14) ),
+        new Quote(1, 'Anonymous','I used to think i was indecisive but now im not too sure',new Date(2019,1,1) ),
+        new Quote(2, 'Anonymous','Life is short smile while you still have teeth',new Date(2019,1,2) ),
+        new Quote(3, 'Anonymous','Always remember that you are unique just like everyone else',new Date(2019,1,3) ),
+        new Quote(4, 'Anonymous','People say nothing is impossible but i do nothing every day',new Date(2019,1,4) ),
 
     ]
 
