dddd4c3b3022eced48a77226e24dac4a15eacfdf
Francis Karagu <fkaragu@gmail.com>
2019-01-14 15:31:41 +0300

    Added alignment and indentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Quote} from './quote'
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
@@ -7,11 +7,11 @@ import {Quote} from './quote'
 })
 export class AppComponent {
   quote = [
-        new Quote(1, 'Anonymous','I used to think i was indecisive but now im not too sure',new Date(2019,1,1) ),
-        new Quote(2, 'Anonymous','Life is short smile while you still have teeth',new Date(2019,1,2) ),
-        new Quote(3, 'Anonymous','Always remember that you are unique just like everyone else',new Date(2019,1,3) ),
-        new Quote(4, 'Anonymous','People say nothing is impossible but i do nothing every day',new Date(2019,1,4) ),
+    new Quote(1, 'Anonymous', 'I used to think i was indecisive but now im not too sure', new Date(2019, 1, 1)),
+    new Quote(2, 'Anonymous', 'Life is short smile while you still have teeth', new Date(2019, 1, 2)),
+    new Quote(3, 'Anonymous', 'Always remember that you are unique just like everyone else', new Date(2019, 1, 3)),
+    new Quote(4, 'Anonymous', 'People say nothing is impossible but i do nothing every day', new Date(2019, 1, 4)),
 
-    ]
+  ]
 
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Quote} from '../quote'
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -9,39 +9,39 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
 
   quote = [
-        new Quote(1, 'Anonymous','I used to think i was indecisive but now im not too sure',new Date(2019,1,1) ),
-        new Quote(2, 'Anonymous','Life is short smile while you still have teeth',new Date(2019,1,2) ),
-        new Quote(3, 'Anonymous','Always remember that you are unique just like everyone else',new Date(2019,1,3) ),
-        new Quote(4, 'Anonymous','People say nothing is impossible but i do nothing every day',new Date(2019,1,4) ),
+    new Quote(1, 'Anonymous', 'I used to think i was indecisive but now im not too sure', new Date(2019, 1, 1)),
+    new Quote(2, 'Anonymous', 'Life is short smile while you still have teeth', new Date(2019, 1, 2)),
+    new Quote(3, 'Anonymous', 'Always remember that you are unique just like everyone else', new Date(2019, 1, 3)),
+    new Quote(4, 'Anonymous', 'People say nothing is impossible but i do nothing every day', new Date(2019, 1, 4)),
 
-    ]
+  ]
 
-    addNewQuote(quote){
+  addNewQuote(quote) {
     let quoteLength = this.quote.length;
-    quote.id=quoteLength+1;
+    quote.id = quoteLength + 1;
     quote.completeDate = new Date(quote.completeDate)
     this.quote.push(quote)
 
-}
-
-  toogleDetails(index){
-      this.quote[index].showDescription = !this.quote[index].showDescription;
   }
-  deleteQuote(isComplete,index){
-        if (isComplete){
-            let toDelete=confirm(`Are you sure you want to delete ${this.quote[index].name}`)
 
-            if(toDelete){
-                this.quote.splice(index,1)
-            }
-        }
+  toogleDetails(index) {
+    this.quote[index].showDescription = !this.quote[index].showDescription;
+  }
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quote[index].name}`)
+
+      if (toDelete) {
+        this.quote.splice(index, 1)
+      }
     }
+  }
 
-  completeQuote(isComplete,index){
-    if (isComplete){
-        this.quote.splice(index,1);
-        }
-        }
+  completeQuote(isComplete, index) {
+    if (isComplete) {
+      this.quote.splice(index, 1);
+    }
+  }
 
   constructor() { }
 
